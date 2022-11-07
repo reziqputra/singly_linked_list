979192d1ea6000454958011045d9645a161ffe36
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-07 18:10:20 +0800

    public void addnote

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -21,11 +21,42 @@ namespace singly_linked_list
         {
             int rollNo;
             string nm;
-            Console.Write();
+            Console.Write("\nEnter the roll Number of the student: ");
             rollNo = Convert.ToInt32(Console.ReadLine());
-            Console.Write();
+            Console.Write("\nENter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollnumber = rollNo;
+            newnode.name = nm;
+            if((START == null) || rollNo <=START.rollnumber)
+            {
+                if ((START != null)&& (rollNo == START.rollnumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
+
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null)&&(rollNo >= current.rollnumber))
+            {
+                if(rollNo == current.rollnumber)
+                {
+                    Console.WriteLine();
+                    return ;
+                }
+            }
+            newnode.next = current;
+            previous.next = current;
         }
     }
+
     class Program
     {
         static void Main(string[]args)
