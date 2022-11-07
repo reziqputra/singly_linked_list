b56ac6284637185b5841317507e2a5b350b07f35
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-07 18:53:15 +0800

    create case 2

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -130,6 +130,22 @@ namespace singly_linked_list
                                 obj.addnote();
                             }
                             break;
+                        case '2':
+                            {
+                                if(obj.listEmpty())
+                                {
+                                    Console.WriteLine("\n List is Empty");
+                                    break;
+                                }
+                                Console.WriteLine("Enter the roll number of" + "the student whose record is to be deleted: ");
+                                int rollNo = Convert.ToInt32(Console.ReadLine());
+                                if(obj.delnode(rollNo) == false)
+                                    Console.WriteLine("\n Record not found.");
+                                else
+                                    Console.WriteLine("Record with roll number" + + rollNo + "deleted");
+                            }
+                            break;
+
                     }
                 }
             }
